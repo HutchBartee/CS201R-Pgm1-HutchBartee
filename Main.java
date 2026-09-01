3fc64d19396df4cb88ee9848d93a94e24120ea95
Bartee <hbk4w@umsystem.edu>
2026-09-01 09:42:57 -0500

    update

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
     
     //create variables
-
     Scanner scanner = new Scanner(System.in);
     int inValue;
 
@@ -20,21 +19,31 @@ public class Main {
 
     //TASK 1:
     //add code to input a string that replaces the value in 'words'
+    System.out.println("Please enter a new value for words: ");
+    words = scanner.nextLine();
 
     //print the string
-
-
+    System.out.println("the new value of words is: " + words);
 
     //TASK 2:
     //add code to input a new value for the variable 'letter'
     //'letter' is defined in line 18
+    System.out.println("Please enter a new value for letter: ");
+    letter = scanner.nextLine().charAt(0);
 
     //TASK 3:
     //if the letter entered is A or B - print "You did GREAT!"
     //if the letter is C - print "You did GOOD!"
     //if the letter is D or F - print "Try again"
     //if the letter is something else, print "Incorrect value"
-
+    if (letter == 'A' || letter == 'B')
+      System.out.println("You did GREAT!!");
+    else if (letter == 'C')
+      System.out.println("You did GOOD!!");
+    else if (letter == 'D' || letter == 'F')
+      System.out.println("Try again");
+    else
+      System.out.println("Incorrect Value");
 
     //LECTURE 1 DEMO:
     //this is demonstrating different values
@@ -42,7 +51,6 @@ public class Main {
 
     System.out.println(bigTheta + "    \u03B1" + "   " + 0b1111);
     
-
     //LECTURE 1 DEMO:
     //formatting values for output
     double value = 16.404674;
